a1ef06d36d4915481d8a393feb8427b8bd459936
krish05s <bhuvakrish75@gmail.com>
2026-01-27 14:29:10 +0530

    modify-server-file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const loginRoutes = require('./routes/loginroutes');
 
 const app = express();
 app.use(cors({origin: 'http://localhost:3000',credentials: true}));
 app.use(express.json());
 
-// Example route to test database connection
+
+app.use("api/", loginRoutes);
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
